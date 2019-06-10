d17c9a9351bd5f2a917016edda51755a278d3072
roman0yurin <gooood85@yandex.ru>
2019-06-10 17:59:14 +0500

    Отладка сборки БД под Windows

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -6079,8 +6079,7 @@ static void read_inheritable_socket(SOCKET *dest, InheritableSocket *src);
 static bool
 save_backend_variables(BackendParameters *param, Port *port)
 #else
-static bool
-save_backend_variables(BackendParameters *param, Port *port,
+bool save_backend_variables(BackendParameters *param, Port *port,
 					   HANDLE childProcess, pid_t childPid)
 #endif
 {
